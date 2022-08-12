5d2f49e10e768e9442c7c1503e587c307274e43a
DFun-programming <102242738+DFun-programming@users.noreply.github.com>
2022-08-12 22:07:07 +0530

    Create Kadane's Algorith.cpp

---
+++ Kadane's Algorith.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(int arr[], int n){
+        int maximum=INT_MIN;
+        int endMax=0;
+        for(int i=0;i<n;i++)
+        {
+            endMax+=arr[i];
+            if(endMax>maximum)
+                maximum=endMax;
+            if(endMax<0)
+                endMax=0;
+            
+        }
+        return maximum;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+        
+        cin>>n; //input size of array
+        
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+            
+        Solution ob;
+        
+        cout << ob.maxSubarraySum(a, n) << endl;
+    }
+}
+
+// } Driver Code Ends
